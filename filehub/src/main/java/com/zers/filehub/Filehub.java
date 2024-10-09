5826126cfb1f71c4f0341152396f0c23f0371b7a
zzzzDenzzzz <12den24@gmail.com>
2024-10-09 09:05:37 +0300

    first commit

---
+++ filehub/src/main/java/com/zers/filehub/Filehub.java
@@ -0,0 +1,13 @@
+package com.zers.filehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Filehub {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Filehub.class, args);
+	}
+
+}

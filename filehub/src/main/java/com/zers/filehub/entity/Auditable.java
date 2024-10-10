86ce9960f4c7e18371e650796ad76d0b687510c8
zzzzDenzzzz <12den24@gmail.com>
2024-10-10 13:32:46 +0300

    add class entity.Auditable

---
+++ filehub/src/main/java/com/zers/filehub/entity/Auditable.java
@@ -0,0 +1,59 @@
+package com.zers.filehub.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.util.AlternativeJdkIdGenerator;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createAt", "updateAt"}, allowGetters = true)
+public abstract class Auditable {
+    @Id
+    @SequenceGenerator(name = "primary_key_seq", sequenceName = "primary_key_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "primary_key_seq")
+    @Column(name = "id", updatable = false)
+    private Long id;
+    private String referenceId = new AlternativeJdkIdGenerator().generateId().toString();
+    @NotNull
+    private Long createdBy;
+    @NotNull
+    private Long updatedBy;
+    @NotNull
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @CreatedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void beforePersist() {
+        var userId = 1L;
+        if (userId == null) {
+            throw new ApiException("Cannot persist entity without user ID in Request Context for this thread");
+        }
+        setCreatedAt(LocalDateTime.now());
+        setCreatedBy(userId);
+        setUpdatedBy(userId);
+        setUpdatedAt(LocalDateTime.now());
+    }
+
+    @PreUpdate
+    public void beforeUpdate() {
+        var userId = 1L;
+        if (userId == null) {
+            throw new ApiException("Cannot update entity without user ID in Request Context for this thread");
+        }
+        setUpdatedAt(LocalDateTime.now());
+        setUpdatedBy(userId);
+    }
+}

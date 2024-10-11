29b590b6d481c41d03d4541c7f0017a9f6cd2ff3
zzzzDenzzzz <12den24@gmail.com>
2024-10-11 09:47:50 +0300

    add class enumiration.converter.RoleConverter

---
+++ filehub/src/main/java/com/zers/filehub/enumiration/converter/RoleConverter.java
@@ -0,0 +1,30 @@
+package com.zers.filehub.enumiration.converter;
+
+import com.zers.filehub.enumiration.Authority;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.util.stream.Stream;
+
+@Converter(autoApply = true)
+public class RoleConverter implements AttributeConverter<Authority, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Authority authority) {
+        if (authority == null) {
+            return null;
+        }
+        return authority.getValue();
+    }
+
+    @Override
+    public Authority convertToEntityAttribute(String code) {
+        if (code == null) {
+        return null;
+    }
+        return Stream.of(Authority.values())
+                .filter(authority -> authority.getValue().equals(code))
+                .findFirst()
+                .orElseThrow(IllegalAccessError::new);
+    }
+}

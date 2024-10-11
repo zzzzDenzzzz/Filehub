3275bdc405d0c59094d98f65eca1b907dc3ae261
zzzzDenzzzz <12den24@gmail.com>
2024-10-11 08:39:46 +0300

    add class entity.UserEntity

---
+++ filehub/src/main/java/com/zers/filehub/entity/UserEntity.java
@@ -0,0 +1,42 @@
+package com.zers.filehub.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+public class UserEntity extends Audittable{
+    @Column(updatable = false, unique = true, nullable = false)
+    private String userId;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true, nullable = false)
+    private String email;
+    private Integer loginAttempts;
+    private LocalDateTime lastLogin;
+    private String phone;
+    private String bio;
+    private String imageUrl;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean enabled;
+    private boolean mfa;
+    @JsonIgnore
+    private String qrCodeSecret;
+    @Column(columnDefinition = "TEXT")
+    private String qrCodeImageUri;
+    private String roles;//TODO create Role class and map here with JPA
+}

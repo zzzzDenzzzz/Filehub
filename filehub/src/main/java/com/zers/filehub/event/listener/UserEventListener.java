6f6f2de792a6f4ab53cee2e932ecf81ed5f830f5
zzzzDenzzzz <12den24@gmail.com>
2024-10-11 13:04:15 +0300

    add docker.compose.yml docker..env

--- filehub/src/main/java/com/zers/filehub/event/listener/UserEventListener.java
+++ filehub/src/main/java/com/zers/filehub/event/listener/UserEventListener.java
@@ -1,4 +1,30 @@
 package com.zers.filehub.event.listener;
 
+import com.zers.filehub.event.UserEvent;
+import com.zers.filehub.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class UserEventListener {
+    private final EmailService emailService;
+
+    public void onUserEvent(UserEvent event) {
+        switch (event.getType()) {
+            case REGISTRATION ->
+                    emailService.sendNewAccountEmail(
+                            event.getUser().getFirstName(),
+                            event.getUser().getEmail(),
+                            event.getData().get("key").toString()
+                    );
+            case RESETPASSWORD ->
+                    emailService.sendPasswordResetEmail(
+                            event.getUser().getFirstName(),
+                            event.getUser().getEmail(),
+                            event.getData().get("key").toString()
+                    );
+            default -> {}
+        }
+    }
 }

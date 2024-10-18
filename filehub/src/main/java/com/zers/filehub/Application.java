4079dfda00ac0e85bdd06b0fcfc93d9503c6a1e0
zzzzDenzzzz <12den24@gmail.com>
2024-10-18 09:41:31 +0300

    fix data.sql

--- filehub/src/main/java/com/zers/filehub/Application.java
+++ filehub/src/main/java/com/zers/filehub/Application.java
@@ -1,13 +1,39 @@
 package com.zers.filehub;
 
+import com.zers.filehub.domain.RequestContext;
+import com.zers.filehub.entity.RoleEntity;
+import com.zers.filehub.enumiration.Authority;
+import com.zers.filehub.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(RoleRepository roleRepository) {
+		return args -> {
+			RequestContext.setUserId(0L);
+			var userRole = new RoleEntity();
+			userRole.setName(Authority.USER.name());
+			userRole.setAuthorities(Authority.USER);
+			roleRepository.save(userRole);
+
+			var adminRole = new RoleEntity();
+			adminRole.setName(Authority.ADMIN.name());
+			adminRole.setAuthorities(Authority.ADMIN);
+			roleRepository.save(adminRole);
+			RequestContext.start();
+		};
+	}
 }
